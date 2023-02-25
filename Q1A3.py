c39ad254906f1176ab6e18a9f15091e0379b4957
B Phan <bphan@Bs-MacBook-Pro.local>
2023-02-24 19:07:38 -0700

    writing database class

--- Q1A3.py
+++ Q1A3.py
@@ -1,5 +1,9 @@
+############### Imports ###############
 import sqlite3
+######################################
 
+
+############################# Class ##############################
 class Database:
     def __init__(self, database_name):
         self.conn = sqlite3.connect(f"./{database_name}")
@@ -7,7 +11,28 @@ class Database:
     
     def close_database(self):
         self.conn.close()
-    
 
+    def save_database(self):
+        self.conn.commit()
+
+    def uninformed(self):
+        self.cursor.executescript('''
+            ALTER TABLE Customers
+            DROP PRIMARY KEY;
+
+            ALTER TABLE Sellers
+            DROP PRIMARY KEY;
+
+            ALTER TABLE Order_items
+            DROP PRIMARY KEY;
+
+            ALTER TABLE Orders
+            DROP PRIMARY KEY;
+        ''')
+###################################################################
+
+
+############################# Main ##############################
 if __name__ == "__main__":
     print("----- Done -----")
+################################################################
