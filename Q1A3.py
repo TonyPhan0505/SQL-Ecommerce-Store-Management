cbc711374fb72ec476f5be86bbad968f68d21d05
B Phan <bphan@Bs-MacBook-Pro.local>
2023-02-25 00:51:41 -0700

    finished the Database class

--- Q1A3.py
+++ Q1A3.py
@@ -17,17 +17,100 @@ class Database:
 
     def uninformed(self):
         self.cursor.executescript('''
-            ALTER TABLE Customers
-            DROP PRIMARY KEY;
+            PRAGMA automatic_index = FALSE;
 
-            ALTER TABLE Sellers
-            DROP PRIMARY KEY;
+            ALTER TABLE Customers RENAME TO Old_Customers;
+            ALTER TABLE Sellers RENAME TO Old_Sellers;
+            ALTER TABLE Orders RENAME TO Old_Orders;
+            ALTER TABLE Order_items RENAME TO Old_Order_items;
 
-            ALTER TABLE Order_items
-            DROP PRIMARY KEY;
+            CREATE TABLE Customers ( 
+                customer_id TEXT,
+                customer_postal_code INTEGER
+            );
+            INSERT INTO Customers SELECT * FROM Old_Customers;
 
-            ALTER TABLE Orders
-            DROP PRIMARY KEY;
+            CREATE TABLE Sellers ( 
+                seller_id TEXT,
+                seller_postal_code INTEGER
+            );
+            INSERT INTO Sellers SELECT * FROM Old_Sellers;
+
+            CREATE TABLE Orders ( 
+                order_id TEXT,
+                customer_id TEXT
+            );
+            INSERT INTO Orders SELECT * FROM Old_Orders;
+
+            CREATE TABLE Order_items ( 
+                order_id TEXT,
+                order_item_id INTEGER,
+                product_id TEXT,
+                seller_id TEXT
+            );
+            INSERT INTO Order_items SELECT * FROM Old_Order_items;
+
+            DROP TABLE Old_Order_items;
+            DROP TABLE Old_Orders;
+            DROP TABLE Old_Customers;
+            DROP TABLE Old_Sellers;
+        ''')
+    
+    def self_optimized(self):
+        self.cursor.executescript('''
+            PRAGMA automatic_index = TRUE;
+
+            ALTER TABLE Customers RENAME TO Old_Customers;
+            ALTER TABLE Sellers RENAME TO Old_Sellers;
+            ALTER TABLE Orders RENAME TO Old_Orders;
+            ALTER TABLE Order_items RENAME TO Old_Order_items;
+
+            CREATE TABLE Customers ( 
+                customer_id TEXT,
+                customer_postal_code INTEGER,
+                PRIMARY KEY(customer_id) 
+            );
+            INSERT INTO Customers SELECT * FROM Old_Customers;
+
+            CREATE TABLE Sellers ( 
+                seller_id TEXT,
+                seller_postal_code INTEGER,
+                PRIMARY KEY(seller_id) 
+            );
+            INSERT INTO Sellers SELECT * FROM Old_Sellers;
+
+            CREATE TABLE Orders ( 
+                order_id TEXT,
+                customer_id TEXT,
+                PRIMARY KEY(order_id),
+                FOREIGN KEY(customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
+            );
+            INSERT INTO Orders SELECT * FROM Old_Orders;
+
+            CREATE TABLE Order_items ( 
+                order_id TEXT,
+                order_item_id INTEGER,
+                product_id TEXT,
+                seller_id TEXT,
+                PRIMARY KEY(order_id,order_item_id,product_id,seller_id), 
+                FOREIGN KEY(seller_id) REFERENCES Sellers(seller_id) ON DELETE CASCADE, 
+                FOREIGN KEY(order_id) REFERENCES Orders(order_id) ON DELETE CASCADE
+            );
+            INSERT INTO Order_items SELECT * FROM Old_Order_items;
+
+            DROP TABLE Old_Order_items;
+            DROP TABLE Old_Orders;
+            DROP TABLE Old_Customers;
+            DROP TABLE Old_Sellers;
+        ''')
+    
+    def user_optimized(self):
+        self.cursor.executescript('''
+            CREATE INDEX customer_postal_code_index
+            ON Customers (customer_postal_code);
+
+            CREATE INDEX seller_postal_code_index
+            ON Sellers (seller_postal_code);
         ''')
 ###################################################################
 
